cd05b3cb7a9eda9f55359d1e999d8d11f1f71384
miZyind <mizyind@gmail.com>
2020-09-29 13:26:15 +0700

    feat: add no-empty-interface rule to react config

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -2,5 +2,9 @@ export = {
   extends: './configs/recommended',
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      { allowSingleExtends: true },
+    ],
   },
 };
