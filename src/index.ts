106d6e5c7a6fb08c62daee3d5cd0d0ef43c126b0
miZyind <mizyind@gmail.com>
2021-12-22 14:41:42 +0700

    feat: add React Native support
    BREAKING CHANGE: React Native support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import base from './configs/base';
+import native from './configs/native';
 import next from './configs/next';
 import rules from './rules';
 
-export = { rules, configs: { base, next } };
+export = { rules, configs: { base, native, next } };
