3921724fdcbb39e0b68f578928cffa9e650d2f7a
miZyind <mizyind@gmail.com>
2022-05-04 14:08:54 +0700

    feat: refactor rules and packages

--- src/configs/native.ts
+++ src/configs/native.ts
@@ -1,41 +1,12 @@
 export = {
   env: { 'react-native/react-native': true },
-  settings: {
-    react: { version: 'detect' },
-    'import/resolver': { node: {}, typescript: { alwaysTryTypes: true } },
-  },
   extends: [
-    './configs/base',
-    'plugin:react/all',
-    'plugin:react-hooks/recommended',
+    './configs/react',
     'plugin:react-native/all',
     'plugin:prettier/recommended',
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-empty-interface': [
-      'error',
-      { allowSingleExtends: true },
-    ],
-    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
-    // Unnecessary
-    'react/sort-comp': 'off',
-    'react/jsx-newline': 'off',
-    'react/jsx-sort-props': 'off',
-    'react/jsx-no-literals': 'off',
-    'react/require-optimization': 'off',
-    'react/forbid-component-props': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    // Unnecessary after React 16.14
-    'react/react-in-jsx-scope': 'off',
-    // Unnecessary while using TypeScript
-    'react/require-default-props': 'off',
-    // Verify the list of the dependencies for Hooks like useEffect and similar
-    'react-hooks/exhaustive-deps': 'error',
-    // Context API supports
-    'react/jsx-max-depth': ['error', { max: 5 }],
-    // Unnecessary for React Native
-    'import/namespace': 'off',
-    'react-native/sort-styles': 'off',
+    'import/namespace': 'off', // Unnecessary for React Native
+    'react-native/sort-styles': 'off', // Unnecessary, too strict
   },
 };
--- src/configs/next.ts
+++ src/configs/next.ts
@@ -1,51 +1,30 @@
 export = {
   env: { node: true, browser: true },
-  settings: {
-    react: { version: 'detect' },
-    'import/resolver': { node: {}, typescript: { alwaysTryTypes: true } },
-  },
   extends: [
-    './configs/base',
-    'plugin:react/all',
-    'plugin:react-hooks/recommended',
-    'plugin:@next/next/core-web-vitals',
+    './configs/react',
+    'plugin:@next/next/recommended',
     'plugin:prettier/recommended',
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-empty-interface': [
-      'error',
-      { allowSingleExtends: true },
-    ],
-    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
-    // Unnecessary
-    'react/sort-comp': 'off',
-    'react/jsx-newline': 'off',
-    'react/jsx-sort-props': 'off',
-    'react/jsx-no-literals': 'off',
-    'react/require-optimization': 'off',
-    'react/forbid-component-props': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    // Unnecessary after React 16.14
-    'react/react-in-jsx-scope': 'off',
-    // Unnecessary while using TypeScript
-    'react/require-default-props': 'off',
-    // Verify the list of the dependencies for Hooks like useEffect and similar
-    'react-hooks/exhaustive-deps': 'error',
-    // Context API supports
-    'react/jsx-max-depth': ['error', { max: 5 }],
-    // NextJS strict mode
     '@next/next/no-css-tags': 'error',
     '@next/next/no-sync-scripts': 'error',
     '@next/next/no-html-link-for-pages': 'error',
     '@next/next/no-img-element': 'error',
+    '@next/next/no-head-element': 'error',
     '@next/next/no-unwanted-polyfillio': 'error',
     '@next/next/no-page-custom-font': 'error',
     '@next/next/no-title-in-document-head': 'error',
     '@next/next/google-font-display': 'error',
     '@next/next/google-font-preconnect': 'error',
-    '@next/next/link-passhref': 'error',
     '@next/next/next-script-for-ga': 'error',
+    '@next/next/no-document-import-in-page': 'error',
+    '@next/next/no-head-import-in-document': 'error',
+    '@next/next/no-script-component-in-head': 'error',
+    '@next/next/no-server-import-in-page': 'error',
     '@next/next/no-typos': 'error',
+    '@next/next/no-duplicate-head': 'error',
+    '@next/next/inline-script-id': 'error',
+    '@next/next/no-before-interactive-script-outside-document': 'error',
+    '@next/next/no-assign-module-variable': 'error',
   },
 };
