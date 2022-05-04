b9eb2764d2df82ff24443b7e6d473d28c2de4c62
miZyind <mizyind@gmail.com>
2022-05-04 14:28:31 +0700

    fix: missing import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import base from './configs/base';
 import native from './configs/native';
 import next from './configs/next';
+import react from './configs/react';
 import rules from './rules';
 
-export = { rules, configs: { base, native, next } };
+export = { rules, configs: { base, native, next, react } };
